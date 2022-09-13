1c0bff4506a04646f810c658a88d21d9de40ce59
jeremij94 <jeremij94@gmail.com>
2022-09-13 11:10:56 -0500

    Math Methods

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Methods math operator
+
+
+
+using MethodsExercisePart2;
+
+var sumAns = Methods.Add(2, 4);
+Console.WriteLine(sumAns);
+
+var differenceAns = Methods.Subtract(45, 10);
+Console.WriteLine(differenceAns);
+
+var productAns = Methods.Multiply(7,7);
+Console.WriteLine(productAns);
+
+var quotientAns = Methods.Divide(81, 9);
+Console.WriteLine(quotientAns);
+Console.ReadLine();
